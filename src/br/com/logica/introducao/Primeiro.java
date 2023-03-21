20af47968c321901367fe62a9e00c93ce7f39123
LUCAS SILVA MOREIRA AQUINO <lucas.smaquino@senacsp.edu.br>
2023-03-21 16:22:34 -0300

    Inicio dos estudos em Java

---
+++ src/br/com/logica/introducao/Primeiro.java
@@ -0,0 +1,23 @@
+package br.com.logica.introducao;
+
+import java.util.Scanner;
+
+public class Primeiro {
+
+	public static void main(String[] args) {
+		
+		String nome;
+		
+		Scanner texto = new Scanner(System.in);
+		
+		System.out.println("digite o seu nome:");
+		
+		nome = texto.next();
+			
+		System.out.println("Olá "  + nome);
+		
+		
+				
+   }
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author lucas.smaquino
+ *
+ */
+module Logica {
+}
